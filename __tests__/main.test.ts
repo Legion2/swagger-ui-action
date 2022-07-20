cf74b58c2778c9f935b555d1583aa5197246a19a
Leon Kiefer <leon.k97@gmx.de>
2022-07-20 15:12:42 +0200

    added tests for getSwaggerUIRelease

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,9 @@
 import * as io from '@actions/io';
 import {
   validateSwaggerUIConfig,
-  createSwaggerConfig
+  createSwaggerConfig,
+  getSwaggerUIRelease,
+  Config
 } from '../src/swagger-ui-action';
 
 test('specFile config mode can be selected', async () => {
@@ -29,4 +31,22 @@ test('swaggerConfigFile gets copied correctly (#156)', async () => {
   });
   await expect(await swaggerConfig).toBe('swagger-config.yaml');
   await io.rmRF('/tmp/swagger-config.yaml');
+});
+
+test('getSwaggerUIRelease returns without error for latest', async () => {
+  const swaggerUIVersion = await getSwaggerUIRelease({
+    swaggerUIVersion: '^3.0.0'
+  } as Config);
+  expect(swaggerUIVersion.tag_name).toMatch(/3.*/);
+});
+
+test('getSwaggerUIRelease returns specific version', async () => {
+  const swaggerUIVersion = await getSwaggerUIRelease({
+    swaggerUIVersion: '3.0.0'
+  } as Config);
+  expect(swaggerUIVersion).toMatchObject({
+    tag_name: 'v3.0.0',
+    tarball_url:
+      'https://api.github.com/repos/swagger-api/swagger-ui/tarball/v3.0.0'
+  });
 });
